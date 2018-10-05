c2c2b268bcbace0ee0d6efc38f2be0fcca52e24c
Lance <whatl3y@gmail.com>
2018-10-05 11:06:35 -0400

    Add all images as being binary types

--- src/LambdaHandlers.js
+++ src/LambdaHandlers.js
@@ -2,4 +2,4 @@ import serverless from 'serverless-http'
 import startServer from './libs/startServer'
 
 const app = startServer(false)
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app, { binary: ['image/*'] })
